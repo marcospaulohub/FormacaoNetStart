ce9130f96267809dd283b98ce815c3fc6c58b5b7
Marcos Paulo <marcos.poo@gmail.com>
2025-03-19 10:46:57 -0300

    feat: Lógica inicial.

---
+++ FormacaoNetStart.Encontro06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+int BuscarElemento(int[] elementos, int elementoBuscado)
+{
+    for (int i = 0; i < elementos.Length; i++)
+    {
+        if (elementos[i] == elementoBuscado)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+int[] numeros = [3, 9, 5, 1, 2, 6, 7];
+int numeroBuscado = 22;
+
+var indice = BuscarElemento(numeros, numeroBuscado);
+
+Console.WriteLine($"{(indice >= 0 ? "ENCONTRADO" : "NÃO ENCONTRADO")}");
+
+
+
